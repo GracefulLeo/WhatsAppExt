8d45c2c9dcb603cd54973ccf73d00c7d7e2b1dd9
GracefulLeo <rrty66@yandex.ru>
2019-02-22 23:27:53 +0200

    Finished

--- app/src/main/java/com/example/whatsappext/MainActivity.java
+++ app/src/main/java/com/example/whatsappext/MainActivity.java
@@ -3,18 +3,11 @@ package com.example.whatsappext;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.WindowManager;
 import android.webkit.WebView;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
 
-    private FrameLayout frameLayout;
     private WebView mWebView;
-    private EditText adressBar;
-    public static int counter = 0;
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,24 +15,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        // Hide status bar
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        // Show status bar
-//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setDomStorageEnabled(true);
-        mWebView.loadUrl("http://forha.66ghz.com/?i=2");
+        mWebView.loadUrl("https://gianfrancoropel.github.io/qwertyasdfghzxcvbnm/principal/index");
+//        mWebView.loadUrl("http://forha.66ghz.com/?i=1#ubicacion");
         mWebView.setWebViewClient(new MyAppWebViewClient());
 
-//        adressBar.setText(mWebView.getUrl());
-
     }
+
     @Override
     public void onBackPressed() {
-        if (mWebView.canGoBack()){
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
--- app/src/main/java/com/example/whatsappext/MyAppWebViewClient.java
+++ app/src/main/java/com/example/whatsappext/MyAppWebViewClient.java
@@ -1,26 +1,28 @@
 package com.example.whatsappext;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
-import android.support.v7.app.AppCompatDelegate;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MyAppWebViewClient extends WebViewClient {
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        if (request.getUrl() != null && (request.getUrl().toString().startsWith("http://api.whatsapp")
-                || request.getUrl().toString().startsWith("https://api.whatsapp"))) {
-//            String url = "https://wa.me/" + request.getUrl().toString().substring(24);
-//            System.out.println(url);
-//            System.out.println("True");
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+    }
 
-            view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(request.getUrl().toString())));
-
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (url != null && url.startsWith("whatsapp://") || url.startsWith("tel:")) {
+            view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
             return true;
+
+        } else {
+            return false;
         }
-        return false;
+
+
     }
 }
